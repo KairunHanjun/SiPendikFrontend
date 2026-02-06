c75dcad5772e19ebb8e7f72137ba1bebbff707e8
KairunHanjun <42841197+KairunHanjun@users.noreply.github.com>
2026-02-06 16:09:04 +0700

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://vtlusodnxzwanjjjboct.supabase.co'
-const supabaseKey = "sb_publishable_kjdjityagtlUHcIwBuBkrw_fcPuYPJd"
-export const supabase = createClient(supabaseUrl, supabaseKey)
-
